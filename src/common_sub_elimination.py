92e79970e93fb728dcfce311988ecd2e9e6ee002
Yanyu Yang <48868872+yyyYangLiu@users.noreply.github.com>
2021-03-21 09:33:51 -0400

    Add files via upload

--- src/common_sub_elimination.py
+++ src/common_sub_elimination.py
@@ -2,6 +2,8 @@
 4. Common Subexpression Elimination
 eg. x1:=y+z -> x1:=y+z 
     x2:=y+z   x2:=x1
+    
+Author: Yanyu Yang 20075550
 '''
 
 def common_sub_eliminate(block):
@@ -9,24 +11,30 @@ def common_sub_eliminate(block):
     new_block = []
     
     for i in range(len(block)):
-        value = ''.join(block[i][2:])
-        if value in existed_instruction:
-            new_ins = block[i][0] + ':=' + existed_instruction[value]
-        else:
-            new_ins = ''.join(block[i])
-        existed_instruction[value] = block[i][0]
-        new_block.append(new_ins)
+        r = block[i][0]
+        value = str(block[i][1:]) #key = "["+", 1, 2]", value = r
+        new_ins = []
         
+        if value in existed_instruction:
+            new_ins.append(r)
+            new_ins.append(existed_instruction[value])
+        else:
+            new_ins = block[i]
+            
+        existed_instruction[value] = r
+        new_block.append(new_ins)
+
     return new_block
             
                 
 def main():
     
-    block = [["x",":=","1","+","2"],
-             ["y",":=","cos", "(", "45", ")"],
-             ["z",":=","1","+","2"]
-            ]
-    t1 = common_sub_eliminate(block)
+    test_block = [["x", "+", 1, 2],
+                 ["y", "cos", 45],
+                 ["z","+", 1, 2]]
+    
+    t1 = common_sub_eliminate(test_block)
     print(t1)
+    
 
 main()
